211b347deaad97561e63c27fbb15a3ab23039306
Tuprott991 <nvtu22@clc.fitus.edu.vn>
2025-02-12 10:17:20 +0700

    Update postgre model 2

--- backend/app/database/postgre_model.py
+++ backend/app/database/postgre_model.py
@@ -6,15 +6,34 @@ from decimal import Decimal
 class UserInfo(BaseModel):
     id: int
     user_id: str # ID của người dùng
-    balance: Decimal # Tiền trong tài khoản
+    fullname: str # Tên của người dùng
+    nickname: str # Biệt danh của người dùng
+    gender: str # Giới tính của người dùng
+    country: str # Quốc gia của người dùng
+    address: str # Địa chỉ của người dùng
+    phonenumber: str # Số điện thoại của người dùng
+    edutoken: Decimal # Tiền edutoken trong tài khoản]
+    learntoken: Decimal # Tiền learntoken trong tài khoản
     created_at: datetime = datetime.now()
     updated_at: datetime = datetime.now() 
 
-class ChatHistory(BaseModel):
+class Course(BaseModel):
     id: int
-    thread_id: str
-    question: str
-    answer: str
+    course_id: str # ID của khóa học
+    name: str # Tên của khóa học
+    category: str # Danh mục của khóa học
+    introduction: str # Giới thiệu về khóa học
+    learning_materials_path: str # Đường dẫn tới tài liệu học
+    description: str # Mô tả của khóa học
+    price: Decimal # Giá của khóa học
+    created_at: datetime = datetime.now()
+    updated_at: datetime = datetime.now()
+
+class ChatHistory(BaseModel):
+    id: int # ID của tin nhắn
+    thread_id: str # ID của cuộc trò chuyện
+    question: str # Câu hỏi của người dùng
+    answer: str # Câu trả lời của chatbot
     created_at: datetime = datetime.now()
 
 
