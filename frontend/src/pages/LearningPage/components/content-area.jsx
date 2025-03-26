a7fdef3b62f06970d3ebabd05f9ad8cf432f7391
Nhân Nguyễn Trọng <nhannguyentrong355@gmail.com>
2025-03-26 22:33:08 +0700

    improve UI

--- frontend/src/pages/LearningPage/components/content-area.jsx
+++ frontend/src/pages/LearningPage/components/content-area.jsx
@@ -9,6 +9,9 @@ import {
   Chip,
   Paper,
   Container,
+  Dialog,
+  DialogTitle,
+  DialogContent,
 } from "@mui/material";
 import { CardActionArea, Stack } from "@mui/material";
 import Slider from "react-slick";
@@ -17,15 +20,18 @@ import "slick-carousel/slick/slick-theme.css";
 import { CloudUpload, Link } from "@mui/icons-material";
 import { settings, topics, courses } from "../constants";
 import { motion } from "framer-motion";
+import CloseIcon from "@mui/icons-material/Close";
 
 const options = [
   {
     icon: <CloudUpload fontSize="large" color="primary" />,
+    color: "#1976d2",
     title: "Upload",
     subtitle: "PDF, PPT, DOC, TXT",
   },
   {
     icon: <Link fontSize="large" color="secondary" />,
+    color: "#9c27b0",
     title: "Paste",
     subtitle: "YouTube, Website",
   },
@@ -38,9 +44,10 @@ const TitleSection = ({ title }) => (
 );
 
 const YourCourses = ({ courses }) => {
-  courses = courses.slice(0, 20);
+  const [open, setOpen] = useState(false);
+  const slide_courses = courses.slice(0, 15);
   const [displayedCourses, setDisplayedCourses] = useState(
-    courses.slice(0, 10)
+    slide_courses.slice(0, 10)
   );
   const observer = useRef();
   const lastCourseElementRef = useCallback(
@@ -50,10 +57,10 @@ const YourCourses = ({ courses }) => {
       observer.current = new IntersectionObserver((entries) => {
         if (
           entries[0].isIntersecting &&
-          displayedCourses.length < courses.length
+          displayedCourses.length < slide_courses.length
         ) {
           // Load more courses
-          const nextCourses = courses.slice(
+          const nextCourses = slide_courses.slice(
             displayedCourses.length,
             displayedCourses.length + 10
           );
@@ -68,7 +75,7 @@ const YourCourses = ({ courses }) => {
 
   return (
     <>
-      <TitleSection title={"Your courses"} />
+      <TitleSection title={"Your learning journey"} />
 
       <Box sx={{ width: "100%", mx: "auto", overflow: "hidden" }}>
         <Stack
@@ -77,6 +84,7 @@ const YourCourses = ({ courses }) => {
           flexWrap="wrap"
           gap={1.5}
           justifyContent="center"
+          padding={2}
         >
           {displayedCourses.map((item, index) => {
             // Check if this is the last element for infinite scroll
@@ -108,7 +116,61 @@ const YourCourses = ({ courses }) => {
           })}
         </Stack>
       </Box>
-
+      {courses.length > 15 && (
+        <Typography
+          variant="body1"
+          color="textSecondary"
+          align="center"
+          sx={{
+            mt: 3,
+            cursor: "pointer",
+            ":hover": {
+              color: "primary.main",
+              textDecoration: "underline",
+            },
+          }}
+          onClick={() => setOpen(true)}
+        >
+          View more
+        </Typography>
+      )}
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        fullWidth
+        maxWidth="lg"
+      >
+        <DialogTitle>
+          All Courses
+          <IconButton
+            aria-label="close"
+            onClick={() => setOpen(false)}
+            sx={{ position: "absolute", right: 8, top: 8 }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <Stack
+            direction="row"
+            spacing={1}
+            flexWrap="wrap"
+            gap={1.5}
+            justifyContent="center"
+            padding={2}
+          >
+            {courses.map((item) => (
+              <Chip
+                key={item.id}
+                label={item.title}
+                variant="outlined"
+                color="primary"
+                sx={{ fontWeight: "bold" }}
+              />
+            ))}
+          </Stack>
+        </DialogContent>
+      </Dialog>
       {displayedCourses.length === 0 && (
         <Typography
           variant="body1"
@@ -153,7 +215,10 @@ const ExploreTopics = () => (
 
 const Content = () => (
   <>
-    <TitleSection title={"What do you want to learn now?"} />
+    <Typography variant="h5" fontWeight={700} color="text.primary">
+    What do you want to <Box component="span" color="primary.main">learn</Box> now?
+    </Typography>
+
 
     <Stack
       direction="row"
@@ -162,40 +227,46 @@ const Content = () => (
       mb={4}
       flexWrap="wrap"
     >
-      {options.map(({ icon, title, subtitle }) => (
+      {options.map(({ icon, title, subtitle, color }) => (
         <Card
-          key={title}
-          sx={{ transition: "0.3s", "&:hover": { transform: "scale(1.05)" } }}
-        >
-          <CardActionArea>
-            <Box
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              p={2}
+        key={title}
+        sx={{
+          transition: "0.3s ease-in-out",
+          borderRadius: 3,
+          minWidth: 200,
+          boxShadow: 3,
+          border: `1.75px dashed ${color}`, // Viền đứt đoạn màu xanh nhạt
+          "&:hover": {
+            transform: "scale(1.07)",
+            boxShadow: 6,
+            borderColor: color, // Viền đậm hơn khi hover
+          },
+        }}
+      >
+        <CardActionArea sx={{ p: 2 }}>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <IconButton
+              size="large"
+              aria-label={title}
+              color="primary"
+              sx={{
+                color: "white",
+                p: 2,
+                mb: 1,
+              }}
             >
-              <IconButton size="large" aria-label={title} color="primary">
-                {icon}
-              </IconButton>
-              <Typography
-                variant="h6"
-                mt={1}
-                fontWeight={600}
-                textAlign="center"
-              >
-                {title}
-              </Typography>
-              <Typography
-                variant="body2"
-                color="text.secondary"
-                mt={1}
-                textAlign="center"
-              >
-                {subtitle}
-              </Typography>
-            </Box>
-          </CardActionArea>
-        </Card>
+              {icon}
+            </IconButton>
+            <Typography variant="h6" fontWeight={700} textAlign="center">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" textAlign="center">
+              {subtitle}
+            </Typography>
+          </Box>
+        </CardActionArea>
+      </Card>
+      
       ))}
     </Stack>
   </>
