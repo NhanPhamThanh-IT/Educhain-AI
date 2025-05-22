149d612ab8145e96a0ab69b98b13126f3abf1e9f
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-05-22 21:47:29 +0700

    Update refactor code, improve UI and fix bug, add lazy loading

--- frontend/src/pages/IntroPages/AboutUs/index.jsx
+++ frontend/src/pages/IntroPages/AboutUs/index.jsx
@@ -3,7 +3,7 @@ import { Container, Box } from '@mui/material';
 
 // Importing built-in components
 import Page from '@components/Page';
-import TeamIntroduction from '@sections/AboutUs/TeamIntroduction';
+import TeamIntroduction from './TeamIntroduction';
 
 // Main content of the About Us page
 const MainContent = () => (
@@ -19,7 +19,7 @@ const MainContent = () => (
 export default function AboutUs() {
     return (
         <Page title="About Us">
-            <Box sx={{ background: 'transparent', minHeight: '100vh', pt: 10, pb: 8 }}>
+            <Box sx={{ background: 'transparent', minHeight: '100vh', pt: 10, pb: 8, mt: 8 }}>
                 <MainContent />
             </Box>
         </Page>
