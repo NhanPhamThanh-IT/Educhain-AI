c2cd1c76dbe6f2c6c06306b9bc67846afad7b02f
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-05-24 13:44:31 +0700

    Update new source

--- frontend/src/sections/LearningPage/VideoSection.jsx
+++ frontend/src/sections/LearningPage/VideoSection.jsx
@@ -1,19 +1,19 @@
-import { Typography } from "@mui/material";
+import {Typography} from "@mui/material";
 
 function VideoSection() {
-  return (
-    <>
-      <Typography variant="h6">Learning by Video</Typography>
-      <iframe
-        width="100%"
-        height="500"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        title="Video"
-        frameBorder="0"
-        allowFullScreen
-      ></iframe>
-    </>
-  );
-}
-
+    return (
+      <>
+        <Typography variant="h6">Learning by Video</Typography>
+          <iframe
+            width="100%"
+            height="500"
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+            title="Video"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </>
+    );
+  }
+  
 export default VideoSection;
